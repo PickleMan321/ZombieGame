5b4b1c81fff1a8a13df512aa010bdd74971c1bbe
shadowdrakon99 <cookmj17@bonaventure.edu>
2018-04-14 14:40:29 -0400

    Text Box for Input

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Modal from './Components/Modal'
+import TextInput from './Components/TextInput'
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
       <Modal/>
+      <TextInput/>
         <Text>Open up App.js to start working on your app!</Text>
       </View>
     );
---
+++ Components/TextInput.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import { View, TextInput } from 'react-native';
+
+class TextInputs extends Component {
+  render() {
+    return (
+      <TextInputs
+        {...this.props} // Inherit any props passed to it; e.g., multiline, numberOfLines below
+        editable = {true}
+        maxLength = {40}
+      />
+    );
+  }
+}
+
+export default class Answer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: 'Answer goes here',
+    };
+  }
+
+  // If you type something in the text box that is a color, the background will change to that
+  // color.
+  render() {
+    return (
+     <View style={{
+       backgroundColor: this.state.text,
+       borderBottomColor: '#000000',
+       borderBottomWidth: 1 }}
+     >
+       <Answer
+         multiline = {true}
+         numberOfLines = {4}
+         onChangeText={(text) => this.setState({text})}
+         value={this.state.text}
+       />
+     </View>
+    );
+  }
+}
