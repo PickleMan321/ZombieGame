b3fda4c62ff06087bff664cf2e96814afb8abbac
PickleMan321 <jakelevington@ymail.com>
2018-04-29 16:39:55 -0400

    Hey, script, cool

---
+++ assets/script.js
@@ -0,0 +1,339 @@
+
+
+const sentences = `
+Hour 1
+You began your day like every other day, waking up late, skipping breakfast, panicly running to make it to class on time, and cutting it close by two minutes as your teacher counts attendance.
+
+This was normal for you. This was a habit for you. You never got in trouble for it, so you never bothered to change how your mornings go (despite how many times you tell yourself that you’ll stop going to bed by four).
+
+When it came to school, you were never the brightest, but you were far from dumb. You got good grades, you did what you were told to do; you could be considered, in a teachers eye as, a great student. Sure, they don’t really know you procrastinate with every assignment and that you have mental breakdowns nearly everyday, but hey, you get good grades so you must be doing well.
+
+You did well in pretty much every class you took, except for that one class you hate, calculus. To you, you never really understood the point of learning that stuff; what would you ever need to know any of that in your life? You know how to do taxes, that should be more than enough math to get by in life. Right?
+
+
+
+Hour 4
+
+It’s tough, but you were doing your best to stay awake during your calculus class. You browsed through everything on your phone so there was nothing left to keep your mind entertained. You sunk into your chair with disappointment as you tuned into what your professor was teaching.
+
+Before you even tried to understand what he was talking about, the professor calls out your name.
+Professor: “Hmm, how about you, (playername)? Can you tell me what you got for an answer?” He asked pointing to the board.
+
+
+You look up at the board in hopes to decrypt whatever he wrote on the board, maybe come up with an answer that doesn’t sound like I wasn’t paying attention. Looking at the board, you realize you have no idea what he wrote…
+
+
+You stare at the board long enough, maybe periodically look at your notes (blank paper) in hopes he’ll ask someone else.
+
+
+Choice one:
+It was a good minute of silence of you pretending to try and figure out the problem. This was the longest minute of your life. You hoped he would just move on, but he was just staring at you expecting an answer to come out of your mouth any second. The silence was broken by a loud screech, some sort of explosion, and screaming.
+
+
+
+ Not a moment later, the sound of loud screeching could be heard from outside, an explosion that sounded a little too close for comfort, and people screaming.
+
+
+
+
+The whole class got up from their seats to look out the window. You ran towards the window to check for myself and saw a truck crashed into the side of one of the buildings across from yours. Not only that, but there were bodies on the floor, a lot of bodies. You couldn’t make out any of the faces from where you stood.
+
+There were people on top of each other, screaming? Why…? Your eyes widened as you finally noticed, these kids were being eaten. You were horrified by the sight,  tumbling backwards onto the floor. The entire room was filled with screams and crying as the teacher tried to calm everyone down. What’s happening out there…?
+
+Professor: “Everyone!” The teacher yelled out, “We need to get back in our seats, lock the door, and wait for help.”
+
+But no-one listened, people were panicking. Some people were recording it with their phones, others calling and texting their loved ones about the situation. You had to do something… You decided to bolt out of the classroom; anywhere was safer than on campus. You had to go home, that was safety.
+
+You could hear your professor call out to you, telling you to come. Looking behind you as you ran, you could see him chase after you. What was he thinking?
+
+You quickly go down the stairs, open the door to the courtyard, and realize that the situation was much worse than it was back in class. There were more bodies littered around the campus. Most of the screams outside were all gone, only moaning of the things walking around.
+
+“What are those things? Zombies?” you thought to yourself thinking maybe you’re going a little crazy.
+
+There were too many to count, even worse, there were too many for you to think you could escape from this. You were trapped on campus along with however many students there were left.
+
+From behind you could hear running and “(Player name)!” be screamed out. You turn to see your professor come down the stairs, short of breath.
+
+Professor: “Oh, it’s good you’re ok! You know, “ He stops to take a few deep breaths, “you’re faster than you look!”
+
+Catching his breath finally, he looks out the door you planned on leaving from, shaking his head from left to right.
+
+Professor: “Things don’t look good out there. It’s not safe for us to escape with all those, uh, things? Zombies? Yeah, that works.”
+
+Professor: “We would need some way of getting rid of most of these zombies if we want to be able to evacuate the school…”
+
+You look out the door again and see even more zombies than before. There’s no way… Maybe the military can handle this?
+
+Professor: “Actually…”
+
+He begins to go into deep thought.
+
+Professor: “Yes, yes that might just work!”
+
+You look at him with a confused look.
+
+Professor: “Looks like I still have a lesson to teach you, (Player name)!”
+
+Professor: “It seems like that truck still has a bit of life in it. If we can bring towards the middle of campus, light it, attract most of the zombies towards it, well, that should take care of our problem,”
+
+He says this with a smirk, almost guaranteed it’ll work. Thinking it over, it does seem like a good idea, but you have no idea how you would even begin to execute such a plan.
+
+The professor turns to you.
+
+Professor: “Seems like a bit of calculus should help us with this problem.”
+
+You look at him, now even more confused than ever. Calculus? Is he even taking this seriously?
+
+(Begin explaining the problem)
+
+
+
+
+Chapter 2: Rush for help
+
+Hour 5
+We ran back inside as quickly as we could before the explosion; it was only a matter of seconds before the truck went off. We went back to the door we exited from, keeping away from the glass door. My body was rushing from excitement, any sec-
+
+A loud, painful explosion erupts from the campus, shaking the whole place. No way. It actually worked? You both look outside to see the outcome and to your amazement, almost all the zombies were gone; the campus was, for the most part, clear of zombies. Before you could even smile from your accomplishment, you could hear a pained scream from outside.
+
+You both rush towards the sound to find a man, on the floor holding his leg in pain. There were some pieces of shrapnel from the truck in his left leg. It wasn’t pretty.
+
+You ussure to the professor to get him to the nearest hospital as fast as possible.
+
+Professor: “Oh no, um, well, there should be a hospital a few miles from here. But we would need a car…”
+
+Lucky for you, you drive, and happen to have a car somewhere in the parking lot. You tell him you have a parked car here on campus.
+
+Professor: “Thank goodness. Ok, can you look up google maps on the fastest route there?”
+
+You quickly pull out your phone and open google maps, but it doesn’t work. There seems to be a connection problem? Of all the times, the school wifi has to be bad now. You tell him that you don’t have any connection. He takes his phone out as well.
+
+Professor: “Strange, I don’t either… Something’s not right here.”
+
+He pauses for a few seconds to think.
+
+You look back at the man on the floor and ask for his name.
+
+Man on the floor: “The names, Adam, why do-
+
+He flinches in pain.
+
+Adam: “Oh god, it freaking hurts. I don’t think I can walk.”
+
+Professor: “Ok, this can work.”
+You turn back at the professor and ask what he has in mind.
+
+Professor: “Ok, well…”
+
+He pulls out a map and a pen. (Normally, there would be more to this, like being in the car and the car having a map. But for shortness sake, he just has one).
+
+Professor: “I think we can find the fastest route with this. Yeah. Ok, (Player name), do you remember what integrals are?”
+
+Again with calculus? Really? How would it help this time?
+
+He starts drawing on the map.
+
+Professor: “So, hear me out…”
+
+(Begin explaining problem)
+
+Professor: “Remember that the derivative of distance with respect to time is velocity. If we know the expected speed we can manage on each road, and the length of the road, we can figure out which sequence of roads will get us to the hospital fastest!”
+
+(Display map with four routes, each with a different d and v(t), solve for t).
+
+
+
+
+
+
+Chapter 3: Escape
+
+Hour 13
+
+It wasn’t safe here anymore, the hospital was overrun. We needed to get out of here, but Adam wasn’t fully healed. It was only a matter of time before the zombies come up to the 5th floor where we are. I suggested to the professor that we had to make our move quickly if we were to get out alive. He looked at me, then back at Adam.
+
+Professor: “We can’t leave Adam here, he won’t make it without us.”
+
+You sighed, he had a point. Leaving him here alone, he would die. You sat on a chair by a window. You went into deep thought, you had to come up with something to get out of here.
+
+The professor paced around staring at the floor, occasionally looking up as if he figured it out.
+
+You suggest to him that maybe he should carry Adam, and you’ll lead the way, maybe clear out whatever zombies they run into.
+
+He stares at you, pondering the idea. You could tell by his facial expression that he didn’t like the idea of that plan.
+
+Professor: “That could work, but we’ll need to travel light if we want to evade most confrontations.”
+
+It made sense. If you were to travel carrying too many things, it would surely slow you guys down and get everyone killed. But what could you use that was light enough to not slow you down, but heavy enough to bash through the zombies?
+
+Both you and the professor looked around for anything you could use.
+
+After a few minutes, you both come back to show what you found.
+
+(Give at least 3-5 objects to choose from where the player will need to calculate each object to see which would be the lightest and most effective)
+
+You say: “Let’s take the sledgehammer!”
+
+Professor: “Did you actually do the math?”
+
+“Of course I did… not…”
+
+“A concussion can happen if you hit a zombie’s head with a force of 95 G’s, or 932 Newtons. Force = mass * acceleration, and …” //finish once we figure out how the hell to include calc.
+
+
+
+Hour 15
+
+You could hear screaming from outside. You looked at the professor, nodded, stood up, and peeked through the window. You could see four people running from a couple of zombies. They were all in different locations, maybe a group? Regardless, they didn’t seem to have anything to defend themselves with.
+
+The professor comes from behind you to also see what was happening. Those people didn’t look like they were going to last long out there. We had to help them, you can’t just watch them be eaten. You asked if there was anything you could do to help.
+
+Professor: “Keep in mind that we may not even be able to save them if we try. Then we would be put into danger. And that won’t be good for Adam.”
+
+You bit your lip at the thought of being eaten alive. How that would feel. It wouldn’t be quick…
+
+The professor steps down and finds something to write with. He starts writing on the floor.
+
+Professor: “We can try to calculate their chances of survival. We won’t be able to save them all considering their distance from each other.”
+
+Professor: “If we can figure out who won’t make it, then maybe we can find who to save. This should increase our chances of saving them and ourselves.”
+
+You didn’t like the idea of picking who will live and die, but you thought that this would be the safest bet.
+
+(Start talking about who won’t make. Math and stuff)
+
+
+
+
+
+
+Chapter 4: Raid the Lab
+
+Hour 28
+
+If no one was coming to help, then maybe doing things yourself might actually be the only thing left to do. But how would you even begin to figure out where this all started from? Is that even possible?
+
+You walked back into the room where your professor and Adam were talking. They both look at you, ready for your decision.
+
+Professor: “Well? Have you decided yet?”
+
+You nod. This was the only way. Besides, there weren’t many other options anyway.
+
+Adam: “Alright! Let’s take back this city!”
+
+Professor: “First, we need to figure out where this all started…”
+
+You wonder if calculus would have the answer… So you ask.
+
+He chuckles.
+
+Professor: “Now you’re getting the hang of it. So, I’m thinking of using (math problem, integration? I think)”
+
+You ask him to explain.
+
+(problem talking)
+
+
+Hour 32
+
+Professor: “This isn’t right. It doesn’t make sense. We did the math!”
+
+You wander the around the lab looking, hoping, for an answer. But there was none. In fact, there was nothing here besides empty and deserted equipment.
+
+Adam: “This is so messed up dude! I told you, math doesn’t solve everything. If you guys weren’t so caught with trying to apply math to everything, we wouldn’t be in this mess to begin with!”
+
+He smashes the equipment around him. He kicks some desks and tables in anger.
+
+Adam: “This isn’t some stupid math class! Don’t you get it? This is real life, not some fun little math trip. People are dying out there every second and we’re just wasting our time with dumb math problems.”
+
+You didn’t know how to respond. For most of this, calculus was actually helping, but maybe you did get too caught up with everything. We risked our lives because of math. We could have died.
+
+Adam: “They’re moving quickly, we need to hurry up. If we lived somewhere upstate, maybe this wouldn’t be so bad.”
+
+Professor: “No, no, wait, that’s just it! We didn’t do our calculations correctly. We forgot to add in some factors.”
+
+Adam: “No, stop, haven’t you done enough already?”
+
+The professor grabs some paper from the floor and begins writing down some formulas. You slowly walk up to his writings and notice it’s nearly the same problem we used before coming here, but with some other factors.
+
+Professor: “Adam, you figured it out. This is a city!”
+
+Adam: “And?”
+
+Professor: “We have traffic, we have high population, and so that means… yeah, yeah, that has to be it.”
+
+Adam: “What are you talking about?”
+
+Professor: “The rate of their movement isn’t as we originally calculated because we didn’t factor in where we live.”
+
+You ask what he means by that.
+
+Professor: “(Explains)”
+
+
+
+Chapter 5: I Meant the Other Lab
+
+
+Hour 36
+
+You looked for every way in, but there was none. It was locked up and shut down from all areas. This had to be it though. This had to be the eye of the storm.
+
+Professor: “There doesn’t seem to be a way in that I can find. How about you guys?”
+
+Adam: “Nothing here.”
+
+You shake your head. This can’t be what stops you; a locked door? You looked around, there had to be a way in. There had to be.
+
+Adam: “Well, I could, like, knock the door down?”
+
+Professor: “I doubt you can do that nor do I suggest even trying…”
+
+Adam: “Your loss.”
+
+As they talked on, you notice a truck in the distance, and nearly a clear route towards the lab. You began thinking of a plan…
+
+The professor takes notice of you.
+
+Professor: “What are you thinking, (Player name)?
+
+You explain and lead him towards the truck.
+
+As you both reach the truck, climb in, and check the gas, you realize there isn’t much gas left. Nearly empty. The truck may not even start…
+Professor: “Looks like we’ll need to plan this out. We’re going to need the right distance to the wall without running out of gas…”
+
+Professor: (Explain)
+
+
+Hour 38
+
+You looked for every way in, but there was none. It was locked up and shut down from all areas. This had to be it though. This had to be the eye of the storm.
+
+Professor: “There doesn’t seem to be a way in that I can find. How about you guys?”
+
+Adam: “Nothing here.”
+
+You shake your head. This can’t be what stops you; a locked door? You looked around, there had to be a way in. There had to be.
+
+Adam: “Well, I could, like, knock the door down?”
+
+Professor: “I doubt you can do that nor do I suggest even trying…”
+
+Adam: “Your loss.”
+
+As they talked on, you notice a truck in the distance, and nearly a clear route towards the lab. You began thinking of a plan…
+
+The professor takes notice of you.
+
+Professor: “What are you thinking, (Player name)?
+
+You explain and lead him towards the truck.
+
+As you both reach the truck, climb in, and check the gas, you realize there isn’t much gas left. Nearly empty. The truck may not even start…
+Professor: “Looks like we’ll need to plan this out. We’re going to need the right distance to the wall without running out of gas…”
+
+Professor: (Explain)
+
+`
+
+export default sentences
--- src/Components/ScriptArray.js
+++ src/Components/ScriptArray.js
@@ -1,25 +1,43 @@
 import React from 'react'
 import {View, Text} from 'react-native';
+import sentences from '../../assets/script';
 
-const sentences = [
-  "Hey!",
-  "This is all we have for now",
-   "But don't worry",
-  "More is to come!",
-  "For now, here's a question!",
-  "Hope you enjoyed!"
+const script = sentences.split('\n').filter( line => line!=="").map( line => {
 
-]
+  if(line.indexOf(": ") < 0) return { message: line }
+
+
+  const explodedLine = line.split(": ")
+
+  return {
+    speaker: explodedLine[0],
+    message: explodedLine[1],
+  }
+
+})
+
+
+
+
+
 
 
 export default class extends React.Component {
 
 render(){
+if(this.props.index >= script.length){
+ return <Text>
+ You is done :)
+ </Text>
+}
 
   return(
     <View>
       <Text>
-        {sentences[this.props.index]}
+         {script[this.props.index].speaker}
+      </Text>
+      <Text>
+        {script[this.props.index].message}
       </Text>
     </View>
   )}
