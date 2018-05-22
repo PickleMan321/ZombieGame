9d33e63922c81989f74e09a3d0e318a7026bd4c7
MidnightPineapple <samuel88835@yahoo.com>
2018-05-22 17:43:21 -0400

    changed index.js to have the router

--- src/index.js
+++ src/index.js
@@ -1,27 +1,13 @@
 import React from 'react';
-import { StyleSheet, Text, View, StatusBar } from 'react-native';
-import Textbox from './Components/Textbox';
-import TextInput from './Components/TextInput';
-import ScriptArray from './Components/ScriptArray';
-import Modal from './Components/Modal';
-
+import { View } from 'react-native';
+import Router from './config/router';
 
 export default class Root extends React.Component {
-
-  constructor(){
-    super();
-    this.state = { sentences : 0 };
-  }
-
-  handlescript() {
-    this.setState({sentences: this.state.sentences + 1})
-  }
 
   render() {
     return (
       <View style={styles.container}>
-        <Textbox index = {this.state.sentences} handle = {this.handlescript.bind(this)}/>
-        <Modal index = {this.state.sentences}/>
+        <Router />
       </View>
     );
   }
@@ -30,9 +16,6 @@ export default class Root extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'flex-end',
     marginTop: StatusBar.currentHeight,
   },
 });
