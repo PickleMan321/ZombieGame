a4f9a484f864106e87fcd8da57d469c59b32e9fa
Pickle <Bobbyboberz@gmail.com>
2018-05-28 15:06:19 -0700

    Added questions and their index locations

--- src/Components/Modal.js
+++ src/Components/Modal.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-import { Alert, Modal, Text, TouchableHighlight, View, StyleSheet, TextInput, Image, Button, ScrollView } from 'react-native';
+import { Alert, Modal, Text, TouchableHighlight, View, StyleSheet, TextInput, Image, Button } from 'react-native';
 
 const questions = [
   {
     text: "How fast would we have to be going if we wanted to be able to circle around the truck twice to gather all the zombies around it and then get back to this building?",
     type:"mc",
-    triggerIndex: 42,
+    triggerIndex: 43,
     onCorrect() {
       alert("You got it!");
       return true;
@@ -19,9 +19,9 @@ const questions = [
     ]
   },
   {
-    text: "What is the limit of 1/x as it goes to infinity?",
+    text: "If her acceleration is -.25 m/s^2, she’s currently going at 4 m/s, and she’s 20m ahead of the zombies going .5 m/s behind her, will she reach that building 100 m ahead of her?",
     type:"mc",
-    triggerIndex: 63,
+    triggerIndex: 99,
     onCorrect() {
       alert("You got it!");
       return true;
@@ -35,9 +35,57 @@ const questions = [
     ]
   },
   {
-    text: "How heavy is this?",
+    text: "If she’s going at .4 m/s, and the zombies are 60 m behind her traveling at .5 m/s, will she make it to the bank 120 m in front of her?",
     type:"mc",
-    triggerIndex: 79,
+    triggerIndex: 101,
+    onCorrect() {
+      alert("You got it!");
+      return true;
+    },
+    onWrong:()=>alert("Nope. Try again!"),
+    choices: [
+      { text:"0", correct:true },
+      { text:"1" },
+      { text:"-1" },
+      { text:"I need more info to answer" },
+    ]
+  },
+  {
+    text: "If we model his acceleration as 2(54x^3 - 63x^2 - 54x + 7)/(3x^2 + 1)^3 , his velocity is 2 m/s, and the zombies are currently 15m behind him going .5 m/s, will he make it to the building 200m ahead of him?",
+    type:"mc",
+    triggerIndex: 103,
+    onCorrect() {
+      alert("You got it!");
+      return true;
+    },
+    onWrong:()=>alert("Nope. Try again!"),
+    choices: [
+      { text:"0", correct:true },
+      { text:"1" },
+      { text:"-1" },
+      { text:"I need more info to answer" },
+    ]
+  },
+  {
+    text: "Who should we save?",
+    type:"mc",
+    triggerIndex: 107,
+    onCorrect() {
+      alert("You got it!");
+      return true;
+    },
+    onWrong:()=>alert("Nope. Try again!"),
+    choices: [
+      { text:"The redhead!", correct:true },
+      { text:"The brunette!" },
+      { text:"The man in the wheelchair!" },
+      { text:"I need more info to answer" },
+    ]
+  },
+  {
+    text: "If we take the distance equation to be d(t) = 1/2x^3 - 3x^2 + 4x +.15 m, what is the mass of the hammer we need to give the zombie a concussion and scare it away?",
+    type:"mc",
+    triggerIndex: 89,
     onCorrect() {
       alert("You got it!");
       return true;
@@ -51,11 +99,26 @@ const questions = [
     ]
   },
   {
-    text: "What is the following derivative evaluated at x=3?",
-    image: require("../../assets/question3.png"),
+    text: "Assume that zombies travel at .25m/s on average, and the outbreak started 4 hours before the zombies showed up on campus, how far must the source be?",
+    type:"mc",
+    triggerIndex: 123,
+    onCorrect() {
+      alert("You got it!");
+      return true;
+    },
+    onWrong:()=>alert("Nope. Try again!"),
+    choices: [
+      { text:"3600 meters?", correct:true },
+      { text:"4200 meters?" },
+      { text:"1500 meters?" },
+      { text:"Really far?" },
+    ]
+  },
+  {
+    text: "If we model the of the speed of the zombies as 0.25x^4 -5x^3+2x^2+7x , based on the population geography between Boston and campus, and it taking four hours for the zombies to reach campus, where did this outbreak start??",
     answer: 8,
     type:'num',
-    triggerIndex:2,
+    triggerIndex:146,
     onCorrect() {
       alert("You got it!");
       return true;
@@ -137,10 +200,14 @@ export default class ModalExample extends Component {
     const { choices, type, image, text } = question;
 
     return(
-      <View style={styles.content} >
+      <View style={styles.container}>
+
         { text && <Text style = {styles.questionText}>{text}</Text> }
         { image && <Image source={image} style={styles.questionImage} resizeMode="contain"/> }
         { type === "mc" ? this.renderChoices(question, choices) : this.renderInput(question) }
+
+
+
       </View>
     )
   }
@@ -157,9 +224,7 @@ export default class ModalExample extends Component {
           transparent={false}
           visible={this.state.modalVisible}
           onRequestClose={() => alert("Answer the question before leaving.")}>
-          <ScrollView contentContainerStyle={styles.container}>
           { question && this.renderQuestion(question) }
-          </ScrollView>
         </Modal>
 
 
@@ -170,16 +235,14 @@ export default class ModalExample extends Component {
 
 const styles = StyleSheet.create({
   container:{
+    opacity:100,
+    alignItems: 'center',
+    justifyContent: 'space-around',
     flex:1,
     backgroundColor:'skyblue'
   },
-  content:{
-    alignItems: 'center',
-    justifyContent: 'space-around',
-  },
   choice: {
-    padding:4,
-    margin:5,
+    padding:5,
     borderWidth:1,
     borderColor:'gray',
     width:"100%",
